25f4ba6a6efb2070f743baea6c3794c3f84250d8
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-03-31 17:04:47 +0200

    feat: added findByUserNameWithRoles to find all user info using join fetch using lazy initialization

--- src/main/java/com/Kee/Ecommerce/Repository/UserRepository.java
+++ src/main/java/com/Kee/Ecommerce/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.Kee.Ecommerce.Repository;
 
 import com.Kee.Ecommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +12,12 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Integer> {
 
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u "+
+    "JOIN FETCH u.credential "+
+    "JOIN FETCH u.roles "
+    +"WHERE u.credential.userName= :username")
+    Optional<User> findByUserNameWithRoles(@Param("username") String userName);
     boolean existsByEmail(String email);
 
     boolean existsByCredentialUserName(String userName);
