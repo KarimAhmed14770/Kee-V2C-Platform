ab4d04666fda2cea1052f2390ab2d77799a98575
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-03-31 17:08:29 +0200

    feat: implemented CustomUserDetailsService to grab the user credentials from the db and make it available for spring security authentication manager

---
+++ src/main/java/com/Kee/Ecommerce/service/CustomUserDetailsService.java
@@ -0,0 +1,48 @@
+package com.Kee.Ecommerce.service;
+
+
+import com.Kee.Ecommerce.Repository.UserRepository;
+import com.Kee.Ecommerce.entity.User;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true) //this is
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        /*fetch all userinfo from the db*/
+        User user=userRepository.findByUserNameWithRoles(username)
+                .orElseThrow(()->new UsernameNotFoundException("User Name not found!"));
+
+        /*convert the roles into a list of strings */
+        List<SimpleGrantedAuthority> roles=user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole().name())).toList();
+
+        /*return the user object that holds the info for spring security*/
+        return new org.springframework.security.core.userdetails.User(user.getCredential().getUserName(),
+                user.getCredential().getPassword(),roles);
+
+    }
+}
