495622bc231c845341b5626961a8e1ecfa7bda8d
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-03 09:45:25 +0200

    fix: fixed names of setters/getters in CustomerProfile and SellerProfile entities.

--- src/main/java/com/Kee/Ecommerce/entity/CustomerProfile.java
+++ src/main/java/com/Kee/Ecommerce/entity/CustomerProfile.java
@@ -39,11 +39,11 @@ public class CustomerProfile {
         this.id = id;
     }
 
-    public User getUserId() {
+    public User getUser() {
         return user;
     }
 
-    public void setUserId(User user) {
+    public void setUser(User user) {
         this.user = user;
     }
 
--- src/main/java/com/Kee/Ecommerce/entity/SellerProfile.java
+++ src/main/java/com/Kee/Ecommerce/entity/SellerProfile.java
@@ -44,11 +44,11 @@ public class SellerProfile {
         this.id = id;
     }
 
-    public User getUserId() {
+    public User getUser() {
         return user;
     }
 
-    public void setUserId(User user) {
+    public void setUser(User user) {
         this.user = user;
     }
 
