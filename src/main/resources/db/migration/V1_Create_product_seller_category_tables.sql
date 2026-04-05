50b0eb5dbdf914faa8ca2e58fd639972a122d888
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-05 15:10:29 +0200

    feat(db): implement multi-inventory schema and add stock constraints

--- src/main/resources/db/migration/V1_Create_product_seller_category_tables.sql
+++ src/main/resources/db/migration/V1_Create_product_seller_category_tables.sql
@@ -37,12 +37,11 @@ CREATE TABLE `seller` (
 DROP TABLE IF EXISTS `products`;
 
 CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,-- this id is the address for the database search
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `category_id` int NOT NULL,
   `description` varchar(300) default Null,  
   `price`  DECIMAL(10,2) NOT NULL,
-  `stock`  int default NULL,
   `image_url` varchar(1000) default Null, 
   `seller_id` int NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp,
@@ -53,10 +52,34 @@ CREATE TABLE `products` (
   Constraint `product_category_fk` Foreign key (`category_id`) REFERENCES category(`id`),
   Constraint `product_seller_fk` Foreign key (`seller_id`) REFERENCES seller(`seller_id`),
   INDEX idx_product_price (price),
-  INDEX idx_product_stock (stock),
   FULLTEXT idx_product_description (`description`) -- The FTS index definition
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 
+DROP TABLE IF EXISTS `inventory`;
+
+CREATE TABLE `inventory` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `seller_id` int not null,
+  `name` varchar(100) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp,
+  Primary key(`id`),
+  Constraint `inventory_seller_id_fk` Foreign key (`seller_id`) REFERENCES seller(`seller_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 
+DROP TABLE IF EXISTS `stock`;
+
+CREATE TABLE `stock` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `inventory_id` int not null,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `updated_at` datetime not null default current_timestamp on update current_timestamp,
+  Primary key(`id`),
+  CONSTRAINT `unique_product_per_inventory` UNIQUE (`product_id`, `inventory_id`), -- only 1 stock allowed for a product in 1 inventory
+  Constraint `stock_product_id_fk` Foreign key (`product_id`) REFERENCES products(`id`),
+  Constraint `stock_inventory_id_fk` Foreign key (`inventory_id`) REFERENCES inventory(`id`),
+  INDEX idx_product_stock (quantity)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
