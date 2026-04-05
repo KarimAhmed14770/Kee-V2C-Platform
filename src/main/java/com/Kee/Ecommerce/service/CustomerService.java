abcc77bf713caaab75c129bc47d766c1f59dd76c
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-05 22:12:56 +0200

    remove : customer profile entity,services all

--- src/main/java/com/Kee/Ecommerce/service/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.Kee.Ecommerce.service;
-
-import com.Kee.Ecommerce.dto.*;
-
-public interface CustomerService {
-    CustomerProfileResponse updateCustomerProfile(CustomerProfileRequest customerProfileRequest);
-    CustomerProfileResponse myProfile();
-}
