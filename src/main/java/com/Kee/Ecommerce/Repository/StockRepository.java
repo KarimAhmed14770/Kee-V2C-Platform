7521ef2653bf8c702f42dbae8b8b558a3a0e51ed
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-06 18:48:03 +0200

    feat: Add atomic update to stock with native query

--- src/main/java/com/Kee/Ecommerce/Repository/StockRepository.java
+++ src/main/java/com/Kee/Ecommerce/Repository/StockRepository.java
@@ -2,11 +2,28 @@ package com.Kee.Ecommerce.Repository;
 
 import com.Kee.Ecommerce.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock,Long> {
     Optional<Stock> findByProductIdAndInventoryId(Long productId,Long inventoryId);
+    @Modifying // REQUIRED for UPDATE/DELETE queries
+    @Transactional // Ensures the update is committed correctly
+    @Query(value =
+            "UPDATE stock "+
+            "SET quantity= quantity- :requiredQty "+
+                    "WHERE quantity>= :requiredQty AND product_id= :productId "+
+                    "AND inventory_id= :inventoryId",
+            nativeQuery = true /*basically it tells spring to use raw SQL
+            so that SQL uses table names (stock) and column names (product_id).
+            JPQL (default) uses Class names (Stock) and Variable names (productId).
+            For an atomic math operation like quantity = quantity - X, it's much safer to use Native SQL.
+            */
+    )
+    int decrementProductStock(int requiredQty,Long productId,Long inventoryId);
 }
