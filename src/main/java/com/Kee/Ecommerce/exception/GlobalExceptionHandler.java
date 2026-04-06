ffa50e452589889e64c9ce0ba584943b63215c67
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-06 12:49:06 +0200

    feat: added exceptions (notfound) for product,cartitem ,(emptycart) for cart, (insufficient stock) for stock

--- src/main/java/com/Kee/Ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/Kee/Ecommerce/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.Kee.Ecommerce.exception;
 
 
+import com.Kee.Ecommerce.entity.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -51,6 +52,50 @@ public class GlobalExceptionHandler {
         error.setTimeStamp(System.currentTimeMillis());
 
         return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleException
+            (ProductNotFoundException productNotFoundException){
+        UserErrorResponse error=new UserErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(productNotFoundException.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleException
+            (CartItemNotFoundException cartItemNotFoundException){
+        UserErrorResponse error=new UserErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(cartItemNotFoundException.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleException
+            (InsufficientStockException insufficientStockException){
+        UserErrorResponse error=new UserErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(insufficientStockException.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<UserErrorResponse> handleException
+            (CartEmptyException cartEmptyException){
+        UserErrorResponse error=new UserErrorResponse();
+        error.setStatus(HttpStatus.NO_CONTENT.value());
+        error.setMessage(cartEmptyException.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error,HttpStatus.NO_CONTENT);
     }
 
     @ExceptionHandler
