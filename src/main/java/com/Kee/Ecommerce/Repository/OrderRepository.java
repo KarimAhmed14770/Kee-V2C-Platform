80c8e4716dcbd78192bd45d9f26d2ace6dc6446d
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-07 11:14:48 +0200

    add: customized search query to fetch all product details using JOIN FETCH

--- src/main/java/com/Kee/Ecommerce/Repository/OrderRepository.java
+++ src/main/java/com/Kee/Ecommerce/Repository/OrderRepository.java
@@ -5,12 +5,19 @@ import com.Kee.Ecommerce.enums.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
     Page<Order> findByUserId(Long id, Pageable pageable);
 
     Page<Order> findByStatus(OrderStatus status, Pageable pageable);
+
+    @Query(value="SELECT o FROM Order o " +
+            "JOIN FETCH o.orderItems " +
+            "WHERE o.id= :id")
+    Optional<Order> findByIdWithItemsDetails(Long id);
 }
