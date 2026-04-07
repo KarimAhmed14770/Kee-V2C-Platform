4d02d388a46d7e40e5367906041e6c8cdf268dd3
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-07 19:10:32 +0200

    feat: add check for username and email for CredentialRepository

--- src/main/java/com/Kee/Ecommerce/Repository/CredentialRepository.java
+++ src/main/java/com/Kee/Ecommerce/Repository/CredentialRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CredentialRepository extends JpaRepository<Credential,Long> {
+    boolean existsByEmail(String email);
+    boolean existsByUserName(String userName);
 }
