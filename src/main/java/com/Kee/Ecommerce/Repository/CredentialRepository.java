1c1c4f3a841b1b731c8d38312f3d805ed8cd7a2e
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-08 09:01:08 +0200

    feat: Add search for credential by username

--- src/main/java/com/Kee/Ecommerce/Repository/CredentialRepository.java
+++ src/main/java/com/Kee/Ecommerce/Repository/CredentialRepository.java
@@ -4,8 +4,13 @@ import com.Kee.Ecommerce.entity.Credential;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CredentialRepository extends JpaRepository<Credential,Long> {
     boolean existsByEmail(String email);
     boolean existsByUserName(String userName);
+
+
+    Optional<Credential> findByUserName(String userName);
 }
