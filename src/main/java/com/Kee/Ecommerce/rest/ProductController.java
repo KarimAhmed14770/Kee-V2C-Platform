0fe4e7ffac281a010113bb330e8742a9aa6eec15
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-08 09:06:29 +0200

    feat: Add product controller endpoint

---
+++ src/main/java/com/Kee/Ecommerce/rest/ProductController.java
@@ -0,0 +1,29 @@
+package com.Kee.Ecommerce.rest;
+
+
+import com.Kee.Ecommerce.dto.ProductViewResponse;
+import com.Kee.Ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public  ProductController(ProductService productService){
+        this.productService=productService;
+    }
+
+        @GetMapping("/")
+        public ResponseEntity<Page<ProductViewResponse>> getProductsByDescription(@RequestParam String search, Pageable page){
+            return ResponseEntity.ok(productService.getProductByDescription(search,page));
+        }
+}
