5c8645d5eba08918836aa9a0e5142d944ce8b8af
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-08 09:02:48 +0200

    refactor: refactor CustomerRepository

--- src/main/java/com/Kee/Ecommerce/Repository/CustomerRepository.java
+++ src/main/java/com/Kee/Ecommerce/Repository/CustomerRepository.java
@@ -1,10 +1,10 @@
 package com.Kee.Ecommerce.Repository;
 
 import com.Kee.Ecommerce.entity.Customer;
-import com.Kee.Ecommerce.entity.User;
 import com.Kee.Ecommerce.enums.UserRoles;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,36 +15,17 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
 
-    Optional<Customer> findByEmail(String email);
+
     Optional<Customer> findByCredentialUserName(String userName);
+    Optional<Customer> findByCredentialEmail(String email);
 
-    @Query("SELECT u FROM User u "+
-            "JOIN FETCH u.roles "
-            +"WHERE u.id= :id")
-    Optional<Customer> findByIdWithRoles(@Param("id") Long id);
-
-    @Query("SELECT u FROM User u "+
-    "JOIN FETCH u.credential "+
-    "JOIN FETCH u.roles "
-    +"WHERE u.credential.userName= :username")
-    Optional<Customer> findByUserNameWithRoles(@Param("username") String userName);
-
-    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.role = :roleName")
-    Page<Customer> findAllByRoleName(@Param("roleName") UserRoles roleName, Pageable pageable);
 
     //return all users
 
 
-    boolean existsByEmail(String email);
+    boolean existsByCredentialEmail(String email);
 
     boolean existsByCredentialUserName(String userName);
 
-    /*searches for a user with this id and this role*/
-    boolean existsByIdAndRoles_Role(Long id, UserRoles role);
-
-    @Query("SELECT u FROM User u " +
-            "JOIN FETCH u.roles " +
-            "WHERE u.id IN (SELECT r2.user.id FROM Role r2 WHERE r2.role = :userRole)")
-    Page<Customer> findUsersByRole(@Param("userRole") UserRoles userRole, Pageable pageable);
 
 }
