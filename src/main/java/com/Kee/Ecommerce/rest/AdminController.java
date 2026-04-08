131c2ab5efb88fdc39cefc7ce753695b8c9f9ca2
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-08 14:35:51 +0200

    feat: Add rest endpoints for categories

--- src/main/java/com/Kee/Ecommerce/rest/AdminController.java
+++ src/main/java/com/Kee/Ecommerce/rest/AdminController.java
@@ -1,15 +1,15 @@
 package com.Kee.Ecommerce.rest;
 
-import com.Kee.Ecommerce.dto.CustomerProfileResponse;
-import com.Kee.Ecommerce.dto.PromotionRequest;
-import com.Kee.Ecommerce.dto.CustomerRegistrationResponse;
-import com.Kee.Ecommerce.dto.VendorProfileResponse;
+import com.Kee.Ecommerce.dto.*;
 import com.Kee.Ecommerce.enums.UserRoles;
+import com.Kee.Ecommerce.service.AdminService;
 import com.Kee.Ecommerce.service.AdminServiceImpl;
+import com.Kee.Ecommerce.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -17,43 +17,37 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    private final AdminServiceImpl adminService;
+    private final AdminService adminService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public  AdminController( AdminServiceImpl adminService){
+    public  AdminController( AdminService adminService,CategoryService categoryService){
         this.adminService=adminService;
+        this.categoryService=categoryService;
     }
-/*
-    @PostMapping("/users/promote")
-    public ResponseEntity<String> promoteToSeller(@RequestBody PromotionRequest promotionRequest){
 
-        adminService.promoteToSeller(promotionRequest.userId());
-
-        return ResponseEntity.ok("Promotion successfull for user with id:"+promotionRequest.userId());
-    }
-*/
     @GetMapping("/customers")
-    public ResponseEntity<Page<CustomerProfileResponse>> getAllUsers(Pageable page){
+    public ResponseEntity<Page<CustomerProfileResponse>> getAllCustomers(Pageable page){
         return ResponseEntity.ok(adminService.getAllCustomers(page));
     }
 
     @GetMapping("/vendors")
-    public ResponseEntity<Page<VendorProfileResponse>> getAllSellers(Pageable page){
+    public ResponseEntity<Page<VendorProfileResponse>> getAllVendors(Pageable page){
         return ResponseEntity.ok(adminService.getAllVendors(page));
     }
 
     @GetMapping("/customers/email/{email}")
-    public ResponseEntity<CustomerProfileResponse> getUserByMail(@PathVariable("email")String email){
+    public ResponseEntity<CustomerProfileResponse> getCustomerByMail(@PathVariable("email")String email){
         return ResponseEntity.ok(adminService.getCustomerByEmail(email));
     }
 
     @GetMapping("/customers/id/{id}")
-    public ResponseEntity<CustomerProfileResponse> getUserById(@PathVariable("id") Long id){
+    public ResponseEntity<CustomerProfileResponse> getCustomerById(@PathVariable("id") Long id){
         return ResponseEntity.ok(adminService.getCustomerById(id));
     }
 
     @GetMapping("/customers/user_name/{userName}")
-    public ResponseEntity<CustomerProfileResponse> getUserByUserName(@PathVariable("userName") String userName){
+    public ResponseEntity<CustomerProfileResponse> getCustomerByUserName(@PathVariable("userName") String userName){
         return ResponseEntity.ok(adminService.getCustomerByUsername(userName));
     }
 
@@ -72,7 +66,32 @@ public class AdminController {
         return ResponseEntity.ok(adminService.getVendorByUsername(userName));
     }
 
-    //admin may also want to get seller info
+
+    @GetMapping("/Categories")
+    public ResponseEntity<Page<CategoryResponse>> getAllCategories(Pageable page){
+        return ResponseEntity.ok(categoryService.getAllCategories(page));
+    }
+
+    @PostMapping("/Categories")
+    public ResponseEntity<CategoryResponse> addCategory(@RequestBody CategoryAddRequest categoryAddRequest){
+        return ResponseEntity.ok(categoryService.addCategory(categoryAddRequest));
+    }
+
+    @PatchMapping("/Categories/{id}")
+    public ResponseEntity<CategoryResponse> updateCategory(@PathVariable("id") Long id,
+                                                           @RequestBody CategoryUpdateRequest categoryUpdateRequest){
+        return ResponseEntity.ok(categoryService.updateCategory(id,categoryUpdateRequest));
+    }
+
+    @GetMapping("/Categories/{id}")
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    }
+    @GetMapping("/Categories/{name}")
+    public ResponseEntity<CategoryResponse> getCategoryByName(@PathVariable("name") String name){
+        return ResponseEntity.ok(categoryService.getCategoryByName(name));
+    }
+
 
 
 
