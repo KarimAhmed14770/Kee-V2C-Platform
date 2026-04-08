8d0fff2b041817409f7cd4ab36435e3308c3f9eb
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-08 09:00:20 +0200

    refactor: refactor endpoint url and methods to depend on authService

--- src/main/java/com/Kee/Ecommerce/rest/AuthRestController.java
+++ src/main/java/com/Kee/Ecommerce/rest/AuthRestController.java
@@ -2,42 +2,40 @@ package com.Kee.Ecommerce.rest;
 
 
 import com.Kee.Ecommerce.dto.*;
-import com.Kee.Ecommerce.entity.Credential;
-import com.Kee.Ecommerce.entity.User;
-import com.Kee.Ecommerce.security.UserDetailsImpl;
+import com.Kee.Ecommerce.service.AuthService;
 import com.Kee.Ecommerce.service.JwtService;
-import com.Kee.Ecommerce.service.UserService;
+import com.Kee.Ecommerce.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthRestController {
 
-    private final UserService userService;
+    private final CustomerService customerService;
+    private final AuthService authService;
 
     @Autowired
-    public AuthRestController(UserService userService,AuthenticationManager authenticationManager,
-                              JwtService jwtService){
-        this.userService=userService;
+    public AuthRestController(CustomerService customerService,AuthService authService){
+        this.customerService = customerService;
+        this.authService=authService;
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<UserResponseDTO> register(@RequestBody UserRegistrationDTO userRegistrationDTO){
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(userRegistrationDTO));
+    @PostMapping("/register/customer")
+    public ResponseEntity<CustomerRegistrationResponse> register(@RequestBody CustomerRegistrationDTO customerRegistrationDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerCustomer(customerRegistrationDTO));
+    }
+    @PostMapping("/register/vendor")
+    public ResponseEntity<VendorRegistrationResponse> register(@RequestBody VendorRegistrationDto vendorRegistrationDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerVendor(vendorRegistrationDto));
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> loginProcess(@RequestBody LoginRequest loginRequest){
-        return ResponseEntity.ok(userService.logIn(loginRequest));
+        return ResponseEntity.ok(authService.logIn(loginRequest));
         }
 
 }
