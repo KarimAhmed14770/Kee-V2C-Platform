f6e87ced8b4c3a9668b2bca1978cc135f4e3b6f0
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-09 13:49:04 +0200

    remove: all category functionality from category service, keeping only search for category

--- src/main/java/com/Kee/V2C/service/CategoryServiceImpl.java
+++ src/main/java/com/Kee/V2C/service/CategoryServiceImpl.java
@@ -1,18 +1,16 @@
 package com.Kee.V2C.service;
 
 import com.Kee.V2C.Repository.CategoryRepository;
-import com.Kee.V2C.dto.CategoryAddRequest;
 import com.Kee.V2C.dto.CategoryResponse;
-import com.Kee.V2C.dto.CategoryUpdateRequest;
 import com.Kee.V2C.entity.Category;
 import com.Kee.V2C.exception.CategoryNotFoundException;
-import com.Kee.V2C.exception.ResourceAlreadyExistsException;
 import com.Kee.V2C.mapper.CategoryMapper;
+import com.Kee.V2C.specifications.CategorySpecs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -26,89 +24,20 @@ public class CategoryServiceImpl implements CategoryService {
         this.categoryMapper=categoryMapper;
     }
 
-
     @Override
-    @Transactional
-    public CategoryResponse addCategory(CategoryAddRequest categoryRequest) {
-        if(categoryRepository.existsByNameIgnoreCase(categoryRequest.name())){
-            throw new ResourceAlreadyExistsException("category Already exists ");
-        }
-        Category category=new Category(categoryRequest.name(), categoryRequest.description(), categoryRequest.imageUrl(),
-                categoryRequest.active());
-        categoryRepository.save(category);
+    public Page<CategoryResponse> getCategoryByAttribute(String name,String description,
+                                                         Boolean active,Pageable page){
+        Specification<Category> spec=(root, query, cb) -> cb.conjunction() ;
+        if(name!=null) spec=spec.and(CategorySpecs.hasName(name));
+        if(description!=null) spec=spec.and(CategorySpecs.hasDescription(description));
+        if(active!=null) spec=spec.and(CategorySpecs.hasActive(active));
 
-        return convertCategoryToDto(category);
-    }
+        Page<Category> categories=categoryRepository.findAll(spec,page);
 
-    /*read and create implemented , remaining update, delete*/
-
-    @Override
-    public CategoryResponse getCategoryById(Long id){
-        return  convertCategoryToDto(categoryRepository.findById(id).orElseThrow(
-                ()->new CategoryNotFoundException("Category with id: "+id+" Not found."))
-        );
-    }
-    @Override
-    public CategoryResponse getCategoryByName(String name){
-        return  convertCategoryToDto(categoryRepository.findByName(name).orElseThrow(
-                ()->new CategoryNotFoundException("Category: "+name+" Not found."))
-        );
-    }
-    @Override
-    public Page<CategoryResponse> getAllCategories(Pageable page){
-        Page<Category> categoryPage= categoryRepository.findAll(page);
-        return categoryPage.map(this::convertCategoryToDto);
+        return  categories.map(this::convertCategoryToDto);
     }
 
 
-    @Override
-    public Page<CategoryResponse> getAllActiveCategories(Pageable page) {
-
-        Page<Category> categoryPage= categoryRepository.findAllByActiveTrue(page);
-        return categoryPage.map(this::convertCategoryToDto);
-    }
-    @Override
-    public Page<CategoryResponse> getAllInactiveCategories(Pageable page) {
-
-        Page<Category> categoryPage= categoryRepository.findAllByActiveFalse(page);
-        return categoryPage.map(this::convertCategoryToDto);
-    }
-
-    @Override
-    public Page<CategoryResponse> getCategoryByNameContains(String search,Pageable page){
-        Page<Category> categoryPage= categoryRepository.findByDescriptionContainingIgnoreCase(search,page);
-        return categoryPage.map(this::convertCategoryToDto);
-    }
-
-    @Override
-    public Page<CategoryResponse> getCategoryByDescriptionContains(String search,Pageable page){
-        Page<Category> categoryPage=categoryRepository.findByDescriptionContainingIgnoreCase(search,page);
-        return categoryPage.map(this::convertCategoryToDto);
-    }
-
-    @Override
-    @Transactional
-    public CategoryResponse updateCategory(Long id, CategoryUpdateRequest categoryRequest){
-        Category updatedCategory=getById(id);
-        categoryMapper.updateCategoryFromDto(categoryRequest,updatedCategory);
-        categoryRepository.save(updatedCategory);
-
-        return convertCategoryToDto(updatedCategory);
-    }
-    @Override
-    @Transactional
-    public CategoryResponse softDeleteCategory(Long id){
-        Category category=getById(id);
-        category.setActive(false);
-        categoryRepository.save(category);
-        return convertCategoryToDto(category);
-    }
-
-    private Category getById(Long id){
-        return categoryRepository.findById(id).orElseThrow(
-                ()-> new CategoryNotFoundException("Category not found")
-        );
-    }
     private CategoryResponse convertCategoryToDto(Category category){
         return new CategoryResponse(
                 category.getId(),
