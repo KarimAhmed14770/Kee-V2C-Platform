77acc2873b03e3926953693b680ba346aee8f34c
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-09 13:51:37 +0200

    refactor: rename from CategoryAddRequest to CategoryRequest

--- src/main/java/com/Kee/V2C/dto/CategoryAddRequest.java
+++
@@ -1,7 +0,0 @@
-package com.Kee.V2C.dto;
-
-public record CategoryAddRequest(String name,
-                                 String description,
-                                 String imageUrl,
-                                 boolean active) {
-}
