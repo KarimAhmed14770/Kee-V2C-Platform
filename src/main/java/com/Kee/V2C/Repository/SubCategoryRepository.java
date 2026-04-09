c9f71380dd431e1ed151a6612be81f039b8d1cb5
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-09 16:31:09 +0200

    feat: find subcategories by parent id, remove other functionalities

--- src/main/java/com/Kee/V2C/Repository/SubCategoryRepository.java
+++ src/main/java/com/Kee/V2C/Repository/SubCategoryRepository.java
@@ -5,28 +5,18 @@ import com.Kee.V2C.entity.SubCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory,Long> {
-    Optional<SubCategory> findByName(String Name);
-
     Optional<SubCategory> findById(Long id);
 
-    Page<SubCategory> findByParentCategoryId(Long id,Pageable pageable);
-    Page<SubCategory> findAllByActiveTrue(Pageable pageable);
-
-    Page<SubCategory> findAllByActiveFalse(Pageable pageable);
-
-    // 3. Search for categories
-    Page<SubCategory> findByNameContainingIgnoreCase(String name, Pageable pageable);
-
-    Page<SubCategory> findByDescriptionContainingIgnoreCase(String name, Pageable pageable);
-
-
-    // 4. Quick existence check
-    boolean existsByName(String name);
+    @Query(value = "Select s from SubCategory s "
+    +"Where s.parentCategory.id= :id")
+    Page<SubCategory> findByParentId(@Param("id") Long id,Pageable page);
     boolean existsByNameIgnoreCase(String name);
 }
