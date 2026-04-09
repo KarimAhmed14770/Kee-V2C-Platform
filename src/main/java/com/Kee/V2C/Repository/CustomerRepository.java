5b935a3fce8b19e2f9ac6e31d3acf60433925cb0
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-09 10:23:24 +0200

    feat: add @query to fetch all credentials in one go,add JPASpecificationExecutor for complex query filtering

--- src/main/java/com/Kee/V2C/Repository/CustomerRepository.java
+++ src/main/java/com/Kee/V2C/Repository/CustomerRepository.java
@@ -3,6 +3,8 @@ package com.Kee.V2C.Repository;
 import com.Kee.V2C.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,6 +14,12 @@ public interface CustomerRepository extends JpaRepository<Customer,Long>, JpaSpe
 
 
     Optional<Customer> findByCredentialUserName(String userName);
+
+    @Query(value = "Select c from Customer c" +
+            " JOIN FETCH c.credential " +
+            "WHERE c.id= :id")
+    Optional<Customer> findByIdWithCredentials(@Param("id")Long id);
+
     Optional<Customer> findByCredentialEmail(String email);
 
 
--- src/main/java/com/Kee/V2C/Repository/VendorRepository.java
+++ src/main/java/com/Kee/V2C/Repository/VendorRepository.java
@@ -2,15 +2,26 @@ package com.Kee.V2C.Repository;
 
 
 
+import com.Kee.V2C.entity.Customer;
 import com.Kee.V2C.entity.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface VendorRepository extends JpaRepository<Vendor,Long> {
+public interface VendorRepository extends JpaRepository<Vendor,Long>, JpaSpecificationExecutor<Vendor> {
     Optional<Vendor> findById(Long id);
+
+    @Query(value = "SELECT v from Vendor v "+
+    "JOIN FETCH v.credential "
+    +"WHERE v.id= :id")
+    Optional<Vendor> findByIdWithCredentials(@Param("id")Long id);
+
+
     Optional<Vendor> findByCredentialUserName(String userName);
     Optional<Vendor> findByCredentialEmail(String email);
 }
