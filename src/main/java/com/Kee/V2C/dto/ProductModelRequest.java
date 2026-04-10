a1cafffdf25182f9c433643176dc0be5c23ab75f
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-10 11:31:25 +0200

    feat: add status to ProductModelRequest

--- src/main/java/com/Kee/V2C/dto/ProductModelRequest.java
+++ src/main/java/com/Kee/V2C/dto/ProductModelRequest.java
@@ -1,10 +1,13 @@
 package com.Kee.V2C.dto;
 
+import com.Kee.V2C.enums.ProductModelStatus;
+
 public record ProductModelRequest(Long subCategoryId,
                                   Long brandId,
                                   String name,
                                   String description,
                                   String imageUrl,
                                   Boolean isGlobal,
-                                  Long vendorId) {
+                                  Long vendorId,
+                                  ProductModelStatus status) {
 }
