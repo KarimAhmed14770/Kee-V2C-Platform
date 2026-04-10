f79166f24219a103b35c6b47ffa1e977d3d833b6
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-10 11:31:10 +0200

    feat: add mapper for productmodel for partial updates

---
+++ src/main/java/com/Kee/V2C/mapper/ProductModelMapper.java
@@ -0,0 +1,15 @@
+package com.Kee.V2C.mapper;
+
+import com.Kee.V2C.dto.CustomerProfileRequest;
+import com.Kee.V2C.dto.ProductModelRequest;
+import com.Kee.V2C.entity.Customer;
+import com.Kee.V2C.entity.ProductModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface ProductModelMapper {
+    void updateProductModelFromDto(ProductModelRequest productModelRequest, @MappingTarget ProductModel productModel);
+}
