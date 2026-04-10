c5730cbd32c48c2f559fd6bbeeb5ba25c15b27d3
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-10 16:35:25 +0200

    feat: add ShopMapper for shop partial updates

---
+++ src/main/java/com/Kee/V2C/mapper/ShopMapper.java
@@ -0,0 +1,14 @@
+package com.Kee.V2C.mapper;
+
+import com.Kee.V2C.dto.ShopRequest;
+import com.Kee.V2C.entity.Shop;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface ShopMapper {
+    void updateShopFromDto(ShopRequest shopRequest,
+                                  @MappingTarget Shop shop);
+}
