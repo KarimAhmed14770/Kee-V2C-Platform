e97a75bf06334f719c5a5d0129c5dc1004eb92e7
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-10 11:33:23 +0200

    feat: add jpaExecutor for productModel

--- src/main/java/com/Kee/V2C/Repository/ProductModelRepository.java
+++ src/main/java/com/Kee/V2C/Repository/ProductModelRepository.java
@@ -2,8 +2,10 @@ package com.Kee.V2C.Repository;
 
 import com.Kee.V2C.entity.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductModelRepository extends JpaRepository<ProductModel,Long> {
+public interface ProductModelRepository extends JpaRepository<ProductModel,Long>,
+        JpaSpecificationExecutor<ProductModel> {
 }
