3eb731f8ee7eb0c505acd6f7e31d6f61439b626b
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-13 19:38:48 +0200

    refactor: rename from ProductModelRequest.java to ProductModelRegisterRequest

--- src/main/java/com/Kee/V2C/dto/product/ProductModelRegisterRequest.java
+++ src/main/java/com/Kee/V2C/dto/product/ProductModelRegisterRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
 
-public record ProductModelRequest(
+public record ProductModelRegisterRequest(
                                     @NotNull(message = "SubCategory ID is required")
                                     @Positive(message = "SubCategory ID must be a positive integer")
                                   Long subCategoryId,
