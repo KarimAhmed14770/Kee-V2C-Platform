e9c285f28c7f9780fc8a837222b234aa10074468
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-13 14:50:41 +0200

    feat: add enum for path folders

---
+++ src/main/java/com/Kee/V2C/enums/PathFolder.java
@@ -0,0 +1,22 @@
+package com.Kee.V2C.enums;
+
+public enum PathFolder {
+    MODELS("Models/"),
+    BRANDS("Brands/"),
+    CATEGORIES("Categories/"),
+    SUBCATEGORIES("Subcategories/"),
+    PRODUCT_REQUESTS("ProductRequests/"),
+    VENDORS("Vendors/");
+
+
+    private final String path;
+
+    PathFolder(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+}
