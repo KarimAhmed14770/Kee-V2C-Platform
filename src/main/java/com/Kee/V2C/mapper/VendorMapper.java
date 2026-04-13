1c67514662c14a88d2c4ac979f3ed98090d71ca0
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-13 14:51:50 +0200

    fix: making the vendor mapper ignore the conversion from image file to u=image url

--- src/main/java/com/Kee/V2C/mapper/VendorMapper.java
+++ src/main/java/com/Kee/V2C/mapper/VendorMapper.java
@@ -4,12 +4,14 @@ package com.Kee.V2C.mapper;
 import com.Kee.V2C.dto.vendor.VendorUpdateProfileRequest;
 import com.Kee.V2C.entity.Vendor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring",
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface VendorMapper {
+    @Mapping(target="imageUrl",ignore = true)
     void updateVendorFromDto(VendorUpdateProfileRequest shopRequest,
                              @MappingTarget Vendor vendor);
 }
