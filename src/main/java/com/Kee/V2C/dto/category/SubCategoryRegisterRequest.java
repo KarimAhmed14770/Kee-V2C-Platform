ff01d9eee6467d4fbcbf809abed32163b9ed92ff
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-13 06:00:45 +0200

    feat: add validation to subCategory requests

--- src/main/java/com/Kee/V2C/dto/category/SubCategoryRegisterRequest.java
+++ src/main/java/com/Kee/V2C/dto/category/SubCategoryRegisterRequest.java
@@ -1,7 +1,15 @@
 package com.Kee.V2C.dto.category;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record SubCategoryRegisterRequest(
+                                @NotBlank(message = "Category name is required")
+                                @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+                                @Pattern(regexp = "^[a-zA-Z\\s&]+$", message = "Name can only contain letters, spaces, and '&'")
                                  String name,
+                                @Size(max = 500, message = "Description cannot exceed 500 characters")
                                  String description,
                                  String imageUrl,
                                  boolean active) {
--- src/main/java/com/Kee/V2C/dto/category/SubCategoryUpdateRequest.java
+++ src/main/java/com/Kee/V2C/dto/category/SubCategoryUpdateRequest.java
@@ -1,7 +1,16 @@
 package com.Kee.V2C.dto.category;
 
-public record SubCategoryUpdateRequest(String name,
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record SubCategoryUpdateRequest(
+                                       @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+                                       @Pattern(regexp = "^[a-zA-Z\\s&]+$", message = "Name can only contain letters, spaces, and '&'")
+                                        String name,
+                                       @Size(max = 500, message = "Description cannot exceed 500 characters")
                                        String description,
+
                                        String imageUrl,
                                        boolean active) {
 }
