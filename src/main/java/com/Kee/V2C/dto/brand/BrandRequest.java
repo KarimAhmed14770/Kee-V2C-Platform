cf1f3793d618618c9524ab210dbf1f9abc2e3355
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-13 07:02:37 +0200

    feat: add validation for brand dto

--- src/main/java/com/Kee/V2C/dto/brand/BrandRequest.java
+++ src/main/java/com/Kee/V2C/dto/brand/BrandRequest.java
@@ -1,4 +1,12 @@
 package com.Kee.V2C.dto.brand;
 
-public record BrandRequest(String name, String description, String imageUrl,Boolean active) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record BrandRequest(@NotBlank
+                            @Size(min=2,max=50)
+                           String name,
+                           @Size(max=300) String description,
+                           String imageUrl,
+                           Boolean active) {
 }
