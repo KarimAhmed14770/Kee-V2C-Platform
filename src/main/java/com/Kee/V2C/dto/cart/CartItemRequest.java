5996cbe0b0e83b38410929025ce94f95a650e5ad
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-13 05:42:48 +0200

    feat: add validation to CartItemRequest

--- src/main/java/com/Kee/V2C/dto/cart/CartItemRequest.java
+++ src/main/java/com/Kee/V2C/dto/cart/CartItemRequest.java
@@ -1,4 +1,12 @@
 package com.Kee.V2C.dto.cart;
 
-public record CartItemRequest(Long productId,Integer quantity) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record CartItemRequest(@NotNull(message = "Product ID is required")
+                              @Positive(message = "Product ID must be a positive integer")
+                              Long productId,
+
+                              @NotNull(message = "Quantity change is required")
+                              Integer quantity) {
 }
