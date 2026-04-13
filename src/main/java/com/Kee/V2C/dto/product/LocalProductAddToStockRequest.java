5df6208dad0205bf4d709b618803f8b637b17cc4
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-13 07:03:40 +0200

    remove: unused record

--- src/main/java/com/Kee/V2C/dto/product/LocalProductAddToStockRequest.java
+++
@@ -1,8 +0,0 @@
-package com.Kee.V2C.dto.product;
-
-public record LocalProductAddToStockRequest(
-                                            Long subCategoryId,
-                                            String name,
-                                            String description,
-                                            String imageUrl) {
-}
