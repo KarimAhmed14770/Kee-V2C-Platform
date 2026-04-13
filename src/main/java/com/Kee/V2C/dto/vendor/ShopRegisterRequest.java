143d01fca21f38229db06398f8653d323ca7b2f1
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-13 06:14:08 +0200

    feat: add validation to shop requests dto

--- src/main/java/com/Kee/V2C/dto/vendor/ShopRegisterRequest.java
+++ src/main/java/com/Kee/V2C/dto/vendor/ShopRegisterRequest.java
@@ -1,7 +1,21 @@
 package com.Kee.V2C.dto.vendor;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record ShopRegisterRequest(
-                         String name,
+                            @NotBlank(message = "Shop name is required")
+                            @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+                            @Pattern(regexp = "^[a-zA-Z\\s&]+$", message = "Name can only contain letters, spaces, and '&'")
+                          String name,
+
+                            @NotBlank(message = "Shop address is required")
+                            @Size(min = 10, max = 200, message = "Address is too short. Please provide more details (Street, Building, etc.)")
+                            @Pattern(
+                                    regexp = "^[a-zA-Z0-9\\p{L}\\s\\.,'\\-\\/\\(\\)]+$",
+                                    message = "Address contains invalid special characters"
+                            )
                           String address,
                           Boolean active) {
 }
--- src/main/java/com/Kee/V2C/dto/vendor/ShopUpdateRequest.java
+++ src/main/java/com/Kee/V2C/dto/vendor/ShopUpdateRequest.java
@@ -1,6 +1,16 @@
 package com.Kee.V2C.dto.vendor;
 
-public record ShopUpdateRequest( String name,
-                                 String address,
-                                 Boolean active) {
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record ShopUpdateRequest(@Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+                                @Pattern(regexp = "^[a-zA-Z\\s&]+$", message = "Name can only contain letters, spaces, and '&'")
+                                    String name,
+                                @Size(min = 10, max = 200, message = "Address is too short. Please provide more details (Street, Building, etc.)")
+                                @Pattern(
+                                        regexp = "^[a-zA-Z0-9\\p{L}\\s\\.,'\\-\\/\\(\\)]+$",
+                                        message = "Address contains invalid special characters"
+                                )
+                                String address,
+                                Boolean active) {
 }
