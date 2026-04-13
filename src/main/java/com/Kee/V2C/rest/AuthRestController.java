a3282e6803050cbe34e46bf074621133e2bf1242
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-13 06:51:26 +0200

    feat: add @Valid annotation for authController

--- src/main/java/com/Kee/V2C/rest/AuthRestController.java
+++ src/main/java/com/Kee/V2C/rest/AuthRestController.java
@@ -9,6 +9,7 @@ import com.Kee.V2C.dto.vendor.VendorRegistrationDto;
 import com.Kee.V2C.dto.vendor.VendorRegistrationResponse;
 import com.Kee.V2C.service.AuthService;
 import com.Kee.V2C.service.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,16 +29,16 @@ public class AuthRestController {
     }
 
     @PostMapping("/register/customer")
-    public ResponseEntity<CustomerRegistrationResponse> register(@RequestBody CustomerRegistrationDTO customerRegistrationDTO){
+    public ResponseEntity<CustomerRegistrationResponse> register(@Valid @RequestBody CustomerRegistrationDTO customerRegistrationDTO){
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerCustomer(customerRegistrationDTO));
     }
     @PostMapping("/register/vendor")
-    public ResponseEntity<VendorRegistrationResponse> register(@RequestBody VendorRegistrationDto vendorRegistrationDto){
+    public ResponseEntity<VendorRegistrationResponse> register(@Valid @RequestBody VendorRegistrationDto vendorRegistrationDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerVendor(vendorRegistrationDto));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> loginProcess(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<AuthenticationResponse> loginProcess(@Valid @RequestBody LoginRequest loginRequest){
         return ResponseEntity.ok(authService.logIn(loginRequest));
         }
 
