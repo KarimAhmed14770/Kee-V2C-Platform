895dc5984ee1780f94ccdb1d2b2ae601debd534e
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-04-14 07:45:16 +0200

    feat: add endpoint to view a product

--- src/main/java/com/Kee/V2C/rest/ProductController.java
+++ src/main/java/com/Kee/V2C/rest/ProductController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
@@ -26,4 +23,8 @@ public class ProductController {
         public ResponseEntity<Page<ProductViewResponse>> getProductsByDescription(@RequestParam String search, Pageable page){
             return ResponseEntity.ok(productService.getProductByDescription(search,page));
         }
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductViewResponse> getProductById(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
 }
